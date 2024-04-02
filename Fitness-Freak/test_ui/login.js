dc34f59f7d2a7c28bb0736c7ab8bc2efd273a17e
daniilzam <dzamyatin.2022@scis.smu.edu.sg>
2024-04-02 23:17:31 +0800

    added test ui

---
+++ Fitness-Freak/test_ui/login.js
@@ -0,0 +1,21 @@
+import {auth,signInWithEmailAndPassword}from "../firebase/auth.js";
+
+
+const submit = document.getElementById('submit')
+submit.addEventListener('click',function(event){
+    event.preventDefault()
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
+    signInWithEmailAndPassword(auth,email,password)
+    .then((userCredential) => {
+        //Logged in
+        const user = userCredential.user;
+        // get user id 
+        console.log(user.uid)
+        window.location.href = 'index.html'
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+    })
+})
